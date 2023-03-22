018e9bcffeb90bdb0421a292a433ed0260d811c7
Nadhea <nadhea.120140128@student.itera.ac.id>
2023-03-22 22:57:15 +0700

    UTS PAM

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import React,{ Component, useEffect, useState } from 'react';
+import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './source/Weather.jsx';
+import { countdownHelper } from './source/helper/index.js';
+import * as ImagePicker from 'expo-image-picker'
+import { Constants } from 'expo-constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    state = {
+        fontLoaded: false,
+    };
+
+    componentDidMount() {
+        this._loadFonts();
+    }
+
+    async _loadFonts() {
+        const fonts = {
+            'BarlowCondensed-Black': require('./sourse/assets/fonts/BarlowCondensed-Black.ttf'),
+            'BarlowCondensed-Regular': require('./sourse/assets/fonts/BarlowCondensed-Regular.ttf'),
+        };
+        await Font.loadAsync(fonts);
+        this.setState({ fontLoaded: true });
+        countdownHelper.runtime();
+    }
+
+    render() {
+        if (!this.state.fontLoaded) return null;
+        return (
+            <View style={st.container}>
+                <StatusBar style="auto" />
+                <Weather />
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const st = StyleSheet.create({
+    container: {
+        flex: 1,
+        minHeight: '100%',
+    },
 });
+
+export default App;
